86ee6f5743bb70fda440fc566a1f86265bd3e152
zhuyj17 <firmlyzhu@163.com>
2017-04-01 14:39:49 +0800

    add index

--- bjtquestion/urls.py
+++ bjtquestion/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from questions.views import index
+from questions.views import *
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', index),
+    url(r'^index/$', index),
+    url(r'^login/$', login),
 ]
--- questions/views.py
+++ questions/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from weibo import APIClient
+
+APP_KEY = '2411916390' # app key
+APP_SECRET = '03cb65de1861da7bbab19d68144e6de8' # app secret
+CALLBACK_URL = 'https://github.com/FirmlyReality' # callback url
 
 # Create your views here.
 
 def index(request):
     return render(request,'index.html')
+
+def login(request):
+    client = APIClient(app_key=APP_KEY, app_secret=APP_SECRET, redirect_uri=CALLBACK_URL)
+    url = client.get_authorize_url()
+    return redirect(url)
